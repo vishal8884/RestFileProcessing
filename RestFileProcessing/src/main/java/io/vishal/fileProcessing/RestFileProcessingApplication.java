95b75b8dea12dcb9670498befbcd03a04ac10e77
vishal <vishalambekar459@gmail.com>
2022-11-01 23:27:02 +0530

    firsdt commit

---
+++ RestFileProcessing/src/main/java/io/vishal/fileProcessing/RestFileProcessingApplication.java
@@ -0,0 +1,13 @@
+package io.vishal.fileProcessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFileProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFileProcessingApplication.class, args);
+	}
+
+}
---
+++ RestFileProcessing/src/test/java/io/vishal/fileProcessing/RestFileProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.vishal.fileProcessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFileProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
